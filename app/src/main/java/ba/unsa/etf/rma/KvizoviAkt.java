8b2d88fe2b9f11fceefc0ea63606770fe9ccbe35
kkadusic <kkadusic1@etf.unsa.ba>
2019-03-31 23:27:57 +0200

    Initial commit

---
+++ app/src/main/java/ba/unsa/etf/rma/KvizoviAkt.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KvizoviAkt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kvizovi_akt);
+    }
+}
